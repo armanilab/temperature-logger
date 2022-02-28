9dcc9b9f58dcf2f46a3f342ec479e1ac6a1bd809
Lexie Scholtz <lexiescholtz@gmail.com>
2022-02-28 11:21:25 -0800

    added test to confirm SD card is working

---
+++ logging_test.py
@@ -0,0 +1,27 @@
+# run this script, then disconnect the feather, remove the SD card, and insert
+# the SD card into your computer. There should be a .txt file named "test.txt"
+# that says "Hello world!" in it.
+
+ needed for initializing SPI and CS line connections
+import board
+import busio
+import digitalio
+# import SD card and filesystem access modules
+import adafruit_sdcard
+import storage
+
+# create SPI bus
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+# create chip selection (CS) line
+cs = digitalio.DigitalInOut(board.D10)
+# create microSD card object
+sd_card = adafruit_sdcard.SDCard(spi, cs)
+# create filesystem object
+vfs = storage.VfsFat(sd_card)
+# mount the microSD card filesystem into the CircuitPython file system
+storage.mount(vfs, "/sd") # /sd/ now used as location for sd card files
+
+with open("/sd/test.txt", "w") as file:
+    file.write("Hello world!\n")
+
+print("done!")
