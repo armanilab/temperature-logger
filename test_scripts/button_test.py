52be39a7c0998fdc55a17f1a6d65405c17aef169
Lexie Scholtz <lexiescholtz@gmail.com>
2022-03-04 09:01:52 -0800

    updated to test all buttons

--- test_scripts/button_test.py
+++ test_scripts/button_test.py
@@ -5,16 +5,32 @@ from digitalio import DigitalInOut, Direction, Pull
 led = DigitalInOut(board.LED)
 led.direction = Direction.OUTPUT
 
-button_b = DigitalInOut(board.D6)
-button_b.direction = Direction.INPUT
-button_b.pull=Pull.UP
+a = DigitalInOut(board.D9)
+a.direction = Direction.INPUT
+a.pull = Pull.UP
+
+b = DigitalInOut(board.D6)
+b.direction = Direction.INPUT
+b.pull=Pull.UP
+
+c = DigitalInOut (board.D5)
+c.direction = Direction.INPUT
+c.pull = Pull.UP
 
 debounce_time = 0.2
 last_button_press = 0.0
 button_counter = 0
+
+def press_button(button):
+    led.value = not led.value # switch LED on/off
+    print(button + str(button_counter))
+    last_button_press = time.time()
+    button_counter += 1
+
 while True:
-    if not button_b.value and time.time() - last_button_press > debounce_time:
-        led.value = not led.value
-        print(button_counter)
-        last_button_press = time.time()
-        button_counter += 1
+    if not a.value and time.time() - last_button_press > debounce_time:
+        press_button('a')
+    if not b.value and time.time() - last_button_press > debounce_time:
+        press_button('b')
+    if not c.value and time.time() - last_button_press > debounce_time:
+        press_button('c')
